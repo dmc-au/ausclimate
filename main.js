87ddabb8e7af7893711a24b788c5ac1f73cfc4ea
dmc <d.massey.cole@gmail.com>
2022-10-05 17:38:43 +1100

    Added small battery graph and created big number template

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// // Bar chart
-// new Chart(document.getElementById("bar-chart"), {
-//     type: 'bar',
-//     data: {
-//       labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-//       datasets: [
-//         {
-//           label: "Population (millions)",
-//           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-//           data: [2478,5267,734,784,433]
-//         }
-//       ]
-//     },
-//     options: {
-//       legend: { display: false },
-//       title: {
-//         display: true,
-//         text: 'Predicted world population (millions) in 2050'
-//       }
-//     }
-// });
-
-
-// Total Olympic Medal Count by Country (1896-2016)
 W = "#888"
 A = "#3e95cd";
 
@@ -53,6 +29,7 @@ function make_olympic(countries) {
         }
     });
 };
+
 
 // CO2 Emissions (t) Per Capita 
 d3.csv('Data/ClimateActionTracker/CAT_tCO2e_cap.csv')
@@ -290,6 +267,73 @@ new Chart(document.getElementById("bar-chart-horizontal"), {
       title: {
         display: true,
         text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+
+
+// Australian Small Scale Battery Installations
+d3.csv("Data/NGER/Small Scale/smallscale_battery_numeric.csv")
+    .then(make_battery);
+
+function make_battery(years) {
+    var yearLabels = years.map(function(d) {return d['Installation year']});
+    var batteryData = years.map(function(d) {return d['Total']});
+    var batteryColour = years.map(function(d) {return A});
+
+    var chart = new Chart(document.getElementById("battery"), {
+        type: 'bar',
+        data: {
+          labels: yearLabels,
+          datasets: [
+            {
+              label: "No. New Batteries",
+              backgroundColor: batteryColour,
+              data: batteryData
+            }
+          ]
+        },
+        options: {
+          legend: { display: false },
+          title: {
+            display: true,
+            text: 'No. Australian Small Scale Battery Installations Per Year'
+          }
+        }
+    });
+};
+
+
+// Example Radar Chart
+new Chart(document.getElementById("radar-chart"), {
+    type: 'radar',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "1950",
+          fill: true,
+          backgroundColor: "rgba(179,181,198,0.2)",
+          borderColor: "rgba(179,181,198,1)",
+          pointBorderColor: "#fff",
+          pointBackgroundColor: "rgba(179,181,198,1)",
+          data: [8.77,55.61,21.69,6.62,6.82]
+        }, {
+          label: "2050",
+          fill: true,
+          backgroundColor: "rgba(255,99,132,0.2)",
+          borderColor: "rgba(255,99,132,1)",
+          pointBorderColor: "#fff",
+          pointBackgroundColor: "rgba(255,99,132,1)",
+          pointBorderColor: "#fff",
+          data: [25.48,54.16,7.61,8.06,4.45]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Distribution in % of world population'
       }
     }
 });
